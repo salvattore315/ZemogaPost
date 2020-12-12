a819d933e75c5ee7d3adc6fd7bdb0e80cfbfb485
Salvador <salvattore315@gmail.com>
2020-12-12 16:57:35 -0500

    creating project post

---
+++ ZemogaPost/ZemogaPost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZemogaPost
+//
+//  Created by Salvador Martinez on 12/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
